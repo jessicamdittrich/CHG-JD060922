d57704c20d4676c549488d848b792e077bf1a047
Jessica Dittrich <104876367+jessicamdittrich@users.noreply.github.com>
2022-06-08 14:02:33 -0700

    Started challenge and cannot get it to work so far

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What are your installation instructions?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide your usage information:',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Please select the license applicable:',
+        name: 'license',
+        choices: ['x', 'xx', 'xxx'],
+    },
+    {
+        type: 'input',
+        message: 'Do you have any guidelines for contributing?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What are your test instructions?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Please provide the best way to reach you should there be any questions:',
+        name: 'questions',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    inquirer.prompt([questions]).then((data) => {
+        const fileName = 'README.md';
+
+        fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+}
 
 // Function call to initialize app
 init();
